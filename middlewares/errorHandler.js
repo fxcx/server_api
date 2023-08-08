531c006c02eb928dcc6b12707dabb19774de0c0d
fxcx <87233669+fxcx@users.noreply.github.com>
2023-08-08 18:42:08 -0300

    herrrorhandler con prisma

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,16 +1,28 @@
 import httpStatus from '../helpers/httpStatus.js'
+import { Prisma } from '@prisma/client'
 
 const ERROR_HANDLERS = {
-  defaultError: (res, err) => {
-    res
-      .status(httpStatus.OK)
-      .json({ success: false, message: err.message })
+  P2002: ({ error, response }) => {
+    response.status(httpStatus.BAD_REQUEST).json({
+      success: false,
+      message: 'Unique constraint failed on one or more fields',
+      error: error.message
+    })
+  },
+  defaultError: ({ error, response }) => {
+    response
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ success: false, message: error.message, isFromErrorHandler: true })
   }
 }
 
-const errorHandler = (err, _, res, next) => {
-  const handler = ERROR_HANDLERS[err.name] ?? ERROR_HANDLERS.defaultError
-  handler(res, err)
-}
+const errorHandler = (error, _request, response, _next) => {
+  let option = error.name
 
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    option = error.code
+  }
+  const handler = ERROR_HANDLERS[option] ?? ERROR_HANDLERS.defaultError
+  handler({ response, error })
+}
 export default errorHandler
